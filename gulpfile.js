a4a5fa00e50aa31f6e2530fb02a0656ee0409576
Mohamed Ali <Mohamed@Penny-Appeal-USA-MacBook-Pro.local>
2017-03-11 13:58:46 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var maps = require('gulp-sourcemaps');
+
+// var paths = {
+//   scripts: ['src/js/*.js', '!client/external/**/*.coffee'],
+//   images: 'client/img/**/*'
+// };
+
+var scripts =  ['src/styles/*.js'];
+
+// Lint Task
+gulp.task('lint', function() {
+  var task = scripts.map(function(script) {
+    return gulp.src(script)
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+  })
+});
+
+// Compile Our sass
+gulp.task('sass', function() {
+  return gulp.src('src/styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/stylesheets'))
+    .pipe(autoprefixer({
+        browsers: ['> 5%'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('public/stylesheets'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+  return gulp.src('public/**/*.js')
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('public/javascripts/'))
+    .pipe(rename('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/javascripts/'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+  gulp.watch('src/scripts/*.js', ['lint', 'scripts']);
+  gulp.watch('src/styles/*.scss', ['sass']);
+});
+
+// Default task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
