ec7337bc278303f1643336ce0006419f2274a0f1
Mohamed Ali <Mohamed@Penny-Appeal-USA-MacBook-Pro.local>
2017-03-28 15:24:26 -0400

    Update route for subscribe to remove trailing /

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/subscribe/', subscribe);
+app.use('/subscribe', subscribe);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
