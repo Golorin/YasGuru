82300c78a926514877167089b906f34044a4dcbe
Mohamed Ali <Mohamed@Penny-Appeal-USA-MacBook-Pro.local>
2017-04-13 01:12:00 -0400

    Release v1.1.0 marks the first major update with CMS management of videos.
    + Homepage includes videos from content management system 'Contentful' in order to allow YasGuru team to update the site easily with new videos using Contentful's API.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('underscore');
+var contentful = require('contentful');
+var client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: process.env.CONTENTFUL_SPACE_ID,
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: process.env.CONTENTFUL_TOKEN
+})
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  client.getEntries()
+  .then((entries) => {
+    // console.log(entries.items);
+    // console.log(entries.sys.type);
+    var fields = _.pluck(entries.items, 'fields');
+    var episodes = _.sortBy(fields, 'episodeNumber');
+    var latest = _.last(episodes);
+    res.render('index', {
+      episodes: episodes,
+      latest: latest
+    });
+  });
+
 });
 
 module.exports = router;
